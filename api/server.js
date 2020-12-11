bf7d2bb13ec64dbf058ff684f81a3680b27685df
Fernando Chavez <fjchavez01@gmail.com>
2020-12-11 09:01:21 -0800

    got api running /

--- api/server.js
+++ api/server.js
@@ -1,1 +1,25 @@
 // build your server here
+const express = require('express');
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+
+
+const server = express()
+
+server.use(cors())
+server.use(morgan('dev'))
+server.use(express.json())
+
+
+
+
+
+server.get('/', (_, res) => {
+    res.send(`<h2>API is online<h2>`)
+})
+
+
+
+module.exports = server;
